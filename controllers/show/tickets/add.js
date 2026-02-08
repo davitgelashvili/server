0b40c99a2b6eb574aa2ce99d3bc702650236fb67
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-08 14:36:21 +0400

    feat: შეიქმნა add.js ბილეთების დასაჯავშნად, (მონაცემების შეტანა სატესტოა და მომავალში ორიგინის მოთხოვნების მიხედვით შეიცლება)

--- controllers/show/tickets/add.js
+++ controllers/show/tickets/add.js
@@ -1,5 +1,36 @@
+const { pool } = require("../../../db");
 
 
-module.exports = async () => {
+module.exports = async (req, res) => {
 
+    const { user , amount, ticketId , eventId} = req.body; //TODO:::მომავალში დავამათოთ სხვა ინფორმაციის დაფეტჩვა როცა ზუსტი პარამეტრები გვეცოდინება აპის
+    const origin = req.headers.origin || '';
+    
+    let platform = null;
+    
+    if (origin.includes('tkt.ge')) {
+        platform = 'tkt.ge';
+    } else if (origin.includes('biletebi.ge')) {
+        platform = 'biletebi.ge';
+    }else if (origin.includes('localhost')){
+        platform = 'dev'
+    }else {
+        return res.status(403).json({success : false , message : 'Forbidden'})
+    }
+
+    try{
+
+        const soldAt = new Date().toISOString().slice(0, 19).replace('T', ' '); //იღებს ზუსტ თარიღს როდისაა ბილეთი ნაყიდი
+
+        const [isAlreadyBooked] = await pool.query('select amount from tickets where ticket_id = ? and event_id = ?', [ticketId, eventId])
+
+        if(isAlreadyBooked.length !== 0)return res.status(400).json({success : false, message : 'You Have Already Bought Tickets'})
+
+        await pool.query('insert into tickets (ticket_id, event_id, buyer_id, status, amount, sold_at, platform) values (?, ?, ?, ?, ?, ?, ?)' , [ticketId, eventId,user, 'valid' , amount, soldAt,platform ])
+        return res.status(200).json({success : true, message : 'Tickets Bought Successfully'})
+
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({success: false,message: 'Server error'});
+    }
 }
