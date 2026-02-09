f0d7510b1d3a78ef537575be5c3197af9ee0d96a
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-09 11:26:07 +0400

    refactor: დარეფაქტორდა პლატფორმის წაკითხვის ფუნქცია add.js ში

--- controllers/show/tickets/add.js
+++ controllers/show/tickets/add.js
@@ -5,19 +5,18 @@ const { pool } = require("../../../db");
 module.exports = async (req, res) => {
 
     const { user , amount, ticketId , eventId} = req.body; //TODO:::მომავალში დავამათოთ სხვა ინფორმაციის დაფეტჩვა როცა ზუსტი პარამეტრები გვეცოდინება აპის
-    const origin = req.headers.origin || '';
-    
-    let platform = null;
-    
-    if (origin.includes('tkt.ge')) {
-        platform = 'tkt.ge';
-    } else if (origin.includes('biletebi.ge')) {
-        platform = 'biletebi.ge';
-    }else if (origin.includes('localhost')){
-        platform = 'dev'
-    }else {
-        return res.status(403).json({success : false , message : 'Forbidden'})
-    }
+    const origin = req.headers.origin ?? '';
+
+    const getPlatform = (origin) => {
+        if (origin.includes('tkt.ge')) return 'tkt.ge';
+        if (origin.includes('biletebi.ge')) return 'biletebi.ge';
+        if (origin.includes('localhost')) return 'dev';
+        return null;
+    };
+
+    const platform = getPlatform(origin);
+
+    if (!platform) {return res.status(403).json({success: false,message: 'Forbidden'});}
 
     try{
 
@@ -27,7 +26,7 @@ module.exports = async (req, res) => {
 
         if(isAlreadyBooked.length !== 0)return res.status(400).json({success : false, message : 'You Have Already Bought Tickets'})
 
-        await pool.query('insert into tickets (ticket_id, event_id, buyer_id, status, amount, sold_at, platform) values (?, ?, ?, ?, ?, ?, ?)' , [ticketId, eventId,user, 'valid' , amount, soldAt,platform ])
+        await pool.query('insert into tickets (ticket_id, event_ialtd, buyer_id, status, amount, sold_at, platform) values (?, ?, ?, ?, ?, ?, ?)' , [ticketId, eventId,user, 'valid' , amount, soldAt,platform ])
         return res.status(200).json({success : true, message : 'Tickets Bought Successfully'})
 
     }catch(err){
