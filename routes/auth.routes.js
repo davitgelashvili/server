d669ccd7f8279d9d7170c47441caaffb3c833f55
Davit Gelashvili <datoogelashvili@gmail.com>
2026-02-22 23:12:11 +0400

    continue

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,10 +8,11 @@ const { requireAuth } = require('../middlewares/auth.middleware');
 
 
 const multer = require('multer')
-const upload = multer({dest : '/uploads'})
+// const upload = multer({dest : '/uploads'})
 
 // Public
-router.post('/register', upload.single('avatar'), auth.register);
+// router.post('/register', upload.single('avatar'), auth.register);
+router.post('/register', auth.register);
 router.post('/login', auth.login);
 router.post('/refresh', auth.refresh);
 router.post('/logout', auth.logout);
--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ const clientRoutes = require('./client.routes');
 //login api
 router.use('/auth', authRoutes);
 
-//dashboard apis
+//saller client apis
 router.use('/v1', eventRoutes); //API ტიკეტები ან ბილეთები ჯისთვის
 // (დავამატოთ ვ2 ვერსია აქ მეორე საიტისტვის (tkt.ge || biletebi.ge-სთვის მორგებული))
 router.use('/dashboard', dashboardRoutes) //რაუტი ჩვენი პლატფორმისთვის
