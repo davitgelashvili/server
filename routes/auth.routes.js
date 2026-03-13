6822a1c8418c0fca5769a4e1cef0e3ed24ccce1a
Davit Gelashvili <datoogelashvili@gmail.com>
2026-03-13 16:31:14 +0400

    fix

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,12 +7,12 @@ const auth = require('../controllers/auth');
 const { requireAuth } = require('../middlewares/auth.middleware');
 
 
-const multer = require('multer')
+// const multer = require('multer')
 // const upload = multer({dest : '/uploads'})
 
 // Public
 // router.post('/register', upload.single('avatar'), auth.register);
-router.post('/register', auth.register);
+// router.post('/register', auth.register);
 router.post('/login', auth.login);
 router.post('/refresh', auth.refresh);
 router.post('/logout', auth.logout);
